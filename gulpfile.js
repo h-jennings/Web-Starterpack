c27046fda661aabe6fb957bbfd36e5ead08cc661
Hunter <jenningsdhunter@gmail.com>
2018-11-21 11:45:05 -0500

    adding an image minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const image = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
+
+
+gulp.task('image', () => gulp
+  .src('src/assets/*')
+  .pipe(image())
+  .pipe(gulp.dest('dist/assets')));
+
 
 gulp.task('sass', () => gulp
   .src('src/scss/main.scss')
@@ -21,8 +29,9 @@ gulp.task('browserSync', () => {
 });
 
 // Watchers
-gulp.task('watch', ['browserSync', 'sass'], () => {
+gulp.task('watch', ['browserSync', 'sass', 'image'], () => {
   gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/assets/*');
   gulp.watch('dist/*.html', browserSync.reload);
 });
 
